7d872bb10b49b4b4b2d4334c27b60a55be750119
joedunn123456789 <129883064+joedunn123456789@users.noreply.github.com>
2023-07-17 11:35:32 -0600

    Update sn-business-rule-cxone-api-request-work-item.js
    updated comments

--- sn-business-rule-cxone-api-request-work-item.js
+++ sn-business-rule-cxone-api-request-work-item.js
@@ -1,7 +1,7 @@
 // JavaScript source code
-// production code
+// NiceCXOne and ServiceNow Integration
 (function executeRule(current, previous /*null when async*/) {
-
+// call CXOneAPI (Script Includes)
     var contactId = new sn_csm_workspace.CXoneAPI().requestWorkItem(current);
 
 })(current, previous);
