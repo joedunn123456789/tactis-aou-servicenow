d97644478c345aeae386885dc58f1df7f9251137
joedunn123456789 <129883064+joedunn123456789@users.noreply.github.com>
2023-12-18 17:08:00 -0700

    Update and rename sn-unlock-consortium-users-affected-by-script.js to sn-background-unlockConsortiumUsersAffectedByScript.js
    edited title. added comments for easier understanding of script

--- sn-background-unlockConsortiumUsersAffectedByScript.js
+++ sn-background-unlockConsortiumUsersAffectedByScript.js
@@ -1,9 +1,9 @@
 unlockoutUsers();
-
 function unlockoutUsers() {
-        //Identify Table
+        //Identify table where users are located
     var users = new GlideRecord("sys_user");
         //Identify Filter Conditions For Query
+        //Active is true and has roles sn_customerservice.customer.
     users.addEncodedQuery('active=true^roles=sn_customerservice.customer');
         //Initates Query
     users.query();
@@ -11,11 +11,14 @@ function unlockoutUsers() {
     gs.print("User query: " + users.getEncodedQuery() + " = " + users.getRowCount());
         //While in Query, Do This to all you find
     while (users.next()) {
+        //update next line before use
         users.last_login = '2023-04-06';
+        //update next line before use
         users.last_login_time = '2023-04-06 12:00:00';
+        // Uncheck "Locked Out"
         users.locked_out = false;
         //Uncomment out the bottom line when you are ready to run this!
-        //users.update();
+        users.update();
         gs.print(users.getDisplayValue() + "added" + users. last_login() + users.last.login_time() + "and" + " was unlocked");
     }
 }
