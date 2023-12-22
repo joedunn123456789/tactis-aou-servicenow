85eb30b36968c465736610bb61a84d37b23da5c5
joedunn123456789 <129883064+joedunn123456789@users.noreply.github.com>
2023-12-22 10:35:11 -0700

    Update and rename sn-background-unlockConsortiumUsersAffectedByScript.js to sn-bg-unlockConsortiumUsersAffectedByScript.js
    add comments for clarity

--- sn-bg-unlockConsortiumUsersAffectedByScript.js
+++ sn-bg-unlockConsortiumUsersAffectedByScript.js
@@ -1,3 +1,6 @@
+// Javascript source code
+// Servicenow background script
+
 unlockoutUsers();
 function unlockoutUsers() {
         //Identify table where users are located
@@ -11,14 +14,15 @@ function unlockoutUsers() {
     gs.print("User query: " + users.getEncodedQuery() + " = " + users.getRowCount());
         //While in Query, Do This to all you find
     while (users.next()) {
-        //update next line before use
+        //update next field to the following value
         users.last_login = '2023-04-06';
-        //update next line before use
+        //update next field to the following value
         users.last_login_time = '2023-04-06 12:00:00';
         // Uncheck "Locked Out"
         users.locked_out = false;
-        //Uncomment out the bottom line when you are ready to run this!
+        //Update the record
         users.update();
+        // Inform the user what was done    
         gs.print(users.getDisplayValue() + "added" + users. last_login() + users.last.login_time() + "and" + " was unlocked");
     }
 }
