0de99bc66ab318b5bcdbd50d4b6a34d5b570a0a3
joedunn123456789 <129883064+joedunn123456789@users.noreply.github.com>
2023-12-22 10:31:41 -0700

    Update and rename sn-change-change-request-state-to-closed.js to sn-bg-change-change-request-state-to-closed.js
    added comments for visibility

--- sn-bg-change-change-request-state-to-closed.js
+++ sn-bg-change-change-request-state-to-closed.js
@@ -1,19 +1,22 @@
 // JavaScript source code
-//For Use: Only for when Change Record is not "Normal"
+// For Use: Only for when Change Record is not "Normal"
+// SN Background Script
+
 // Get the Change Request record
 var changeRequest = new GlideRecord('change_request');
-changeRequest.addQuery('number', 'CHG0030049'); // Replace with the actual Change Request number
+//Identify the specific change request to be changed
+changeRequest.addQuery('number', 'CHG0030049'); 
+//Query the Change Request table
 changeRequest.query();
 if (changeRequest.next()) {
-
     // Set the state to Closed
     changeRequest.setValue('state', 3); // State 3 represents the Closed state
+    // Update the Record
     changeRequest.update();
-
+    // Notify the User
     gs.info('Change Request ' + changeRequest.number + ' has been closed.');
-
 }
-
 else {
+    //Notify user that the change request was not found
     gs.error('Change Request not found.');
 }
